9152a913384cd6194b98eda3e749dba4b51be3de
Theboys23456 <krishna93107437@gmail.com>
2025-05-14 23:45:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,9 @@ import time
 load_dotenv()
 
 # ===== WOODcraft ==== SudoR2spr ====
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("21567814"))
+API_HASH = os.getenv("cd7dc5431d449fd795683c550d7bfb7e")
+BOT_TOKEN = os.getenv("7734663845:AAEWTe4hMIhuLdG6RkGElp7NYubpRXbF0JY")
 
 if not API_ID or not API_HASH or not BOT_TOKEN:
     raise ValueError("API_ID, API_HASH, and BOT_TOKEN must be set.")
